18a03792d6139d0834929848da1090b6b3f952ad
Josiah Roa <siahroa25@gmail.com>
2024-07-05 09:49:10 -0600

    Add clerk config with react-router-dom for auth management

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./globals.css";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import RootLayout from "@/layouts/root-layout.tsx";
+import DashboardLayout from "@/layouts/dashboard-layout";
+
+import IndexPage from "@/routes";
+import SignInPage from "@/routes/sign-in";
+import SignUpPage from "@/routes/sign-up";
+import DashboardPage from "@/routes/dashboard";
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <IndexPage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        path: "dashboard",
+        children: [{ path: "/dashboard", element: <DashboardPage /> }],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
